f27bf73d8294e7723e5f1ddbb463995776c56db4
danieljaller <danieljaller@gmail.com>
2017-10-31 15:08:31 +0100

    Added UI project, model, viewModel and service.

---
+++ FriendOrganizer.UI/Data/FriendDataService.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using FriendOrganizer.Model;
+
+namespace FriendOrganizer.UI.Data
+{
+    class FriendDataService : IFriendDataService
+    {
+        public IEnumerable<Friend> GetAllFriends()
+        {
+            yield return new Friend(){FirstName = "Bosse", LastName = "Bildoktorn"};
+            yield return new Friend(){FirstName = "Posse", LastName = "Pildoktorn"};
+            yield return new Friend(){FirstName = "Mosse", LastName = "Mildoktorn"};
+            yield return new Friend(){FirstName = "Sosse", LastName = "Sildoktorn"};
+        }
+    }
+}
