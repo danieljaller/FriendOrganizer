0d5cbb9bfa3be37e2025ad5ca3eea1e0e8d6eb3f
danieljaller <danieljaller@gmail.com>
2017-11-01 15:32:51 +0100

    Added and implemented the OpenFriendDetailsViewEvent

--- FriendOrganizer.UI/ViewModel/FriendDetailsViewModel.cs
+++ FriendOrganizer.UI/ViewModel/FriendDetailsViewModel.cs
@@ -5,16 +5,26 @@ using System.Text;
 using System.Threading.Tasks;
 using FriendOrganizer.Model;
 using FriendOrganizer.UI.Data;
+using FriendOrganizer.UI.Event;
+using Prism.Events;
 
 namespace FriendOrganizer.UI.ViewModel
 {
     public class FriendDetailsViewModel : ViewModelBase, IFriendDetailsViewModel
     {
         private readonly IFriendDataService _dataService;
+        private readonly IEventAggregator _eventAggregator;
 
-        public FriendDetailsViewModel(IFriendDataService dataService)
+        public FriendDetailsViewModel(IFriendDataService dataService, IEventAggregator eventAggregator)
         {
             _dataService = dataService;
+            _eventAggregator = eventAggregator;
+            _eventAggregator.GetEvent<OpenFriendDetailsViewEvent>().Subscribe(OnOpenFriendDetailsView);
+        }
+
+        private async void OnOpenFriendDetailsView(int friendId)
+        {
+            await LoadAsync(friendId);
         }
 
         public async Task LoadAsync(int friendId)
--- FriendOrganizer.UI/ViewModel/NavigationViewModel.cs
+++ FriendOrganizer.UI/ViewModel/NavigationViewModel.cs
@@ -6,16 +6,20 @@ using System.Text;
 using System.Threading.Tasks;
 using FriendOrganizer.Model;
 using FriendOrganizer.UI.Data;
+using FriendOrganizer.UI.Event;
+using Prism.Events;
 
 namespace FriendOrganizer.UI.ViewModel
 {
-    public class NavigationViewModel : INavigationViewModel
+    public class NavigationViewModel : ViewModelBase, INavigationViewModel
     {
         private readonly IFriendLookupDataService _friendLookupService;
+        private readonly IEventAggregator _eventAggregator;
 
-        public NavigationViewModel(IFriendLookupDataService friendLookupService)
+        public NavigationViewModel(IFriendLookupDataService friendLookupService, IEventAggregator eventAggregator)
         {
             _friendLookupService = friendLookupService;
+            _eventAggregator = eventAggregator;
             Friends = new ObservableCollection<LookupItem>();
         }
 
@@ -30,5 +34,20 @@ namespace FriendOrganizer.UI.ViewModel
                 Friends.Add(lookupItem);
             }
         }
+        private LookupItem _selectedFriend;
+
+        public LookupItem SelectedFriend
+        {
+            get { return _selectedFriend; }
+            set
+            {
+                _selectedFriend = value;
+                OnPropertyChanged();
+                if (_selectedFriend != null)
+                    _eventAggregator.GetEvent<OpenFriendDetailsViewEvent>()
+                        .Publish(_selectedFriend.Id);
+            }
+        }
+
     }
 }
