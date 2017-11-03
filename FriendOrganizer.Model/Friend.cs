e567370c01602a18b1d0444746d5789ca135104f
danieljaller <danieljaller@gmail.com>
2017-11-03 14:21:27 +0100

    Added navigation properties from ProgrammingLanguage to Friend.

--- FriendOrganizer.Model/Friend.cs
+++ FriendOrganizer.Model/Friend.cs
@@ -6,13 +6,20 @@ namespace FriendOrganizer.Model
     public class Friend
     {
         public int Id { get; set; }
+
         [Required]
         [MaxLength(50)]
         public string FirstName { get; set; }
+
         [MaxLength(50)]
         public string LastName { get; set; }
+
         [MaxLength(50)]
         [EmailAddress]
         public string Email { get; set; }
+
+        public int? FavoriteLanguageId { get; set; }
+
+        public ProgrammingLanguage FavoriteLanguage { get; set; }
     }
 }
