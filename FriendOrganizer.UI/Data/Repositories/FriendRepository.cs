cd5c9f0bf45f0499498c2d3d6f2a0e45fad994a5
danieljaller <danieljaller@gmail.com>
2017-11-15 14:35:17 +0100

    Refactored into a GenericRepository

--- FriendOrganizer.UI/Data/Repositories/FriendRepository.cs
+++ FriendOrganizer.UI/Data/Repositories/FriendRepository.cs
@@ -5,43 +5,23 @@ using FriendOrganizer.Model;
 
 namespace FriendOrganizer.UI.Data.Repositories
 {
-    class FriendRepository : IFriendRepository
+    class FriendRepository : GenericRepository<Friend, FriendOrganizerDbContext>, IFriendRepository
     {
-        private readonly FriendOrganizerDbContext _context;
-
+        
         public FriendRepository(FriendOrganizerDbContext context)
+            :base(context)
         {
-            _context = context;
+        
         }
 
-        public async Task<Friend> GetByIdAsync(int friendId)
+        public override async Task<Friend> GetByIdAsync(int friendId)
         {
-            return await _context.Friends.Include(f => f.PhoneNumbers).SingleAsync(f => f.Id == friendId);
+            return await Context.Friends.Include(f => f.PhoneNumbers).SingleAsync(f => f.Id == friendId);
         }
-
-        public async Task SaveAsync()
-        {
-            await _context.SaveChangesAsync();
-        }
-
-        public bool HasChanges()
-        {
-            return _context.ChangeTracker.HasChanges();
-        }
-
-        public void Add(Friend friend)
-        {
-            _context.Friends.Add(friend);
-        }
-
-        public void Remove(Friend friendModel)
-        {
-            _context.Friends.Remove(friendModel);
-        }
-
+        
         public void RemovePhoneNumber(FriendPhoneNumber phoneNumber)
         {
-            _context.FriendPhoneNumbers.Remove(phoneNumber);
+            Context.FriendPhoneNumbers.Remove(phoneNumber);
         }
     }
 }
---
+++ FriendOrganizer.UI/Data/Repositories/IGenericRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using FriendOrganizer.Model;
+
+namespace FriendOrganizer.UI.Data.Repositories
+{
+    public interface IGenericRepository<T>
+    {
+        Task<T> GetByIdAsync(int ïd);
+        Task SaveAsync();
+        bool HasChanges();
+        void Add(T model);
+        void Remove(T model);
+    }
+}
