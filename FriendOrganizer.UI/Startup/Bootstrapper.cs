f25bfcb365800fdbe8780f46d6856733e96302d7
danieljaller <danieljaller@gmail.com>
2017-11-15 15:32:18 +0100

    Created the MeetingRepository

--- FriendOrganizer.UI/Startup/Bootstrapper.cs
+++ FriendOrganizer.UI/Startup/Bootstrapper.cs
@@ -35,6 +35,7 @@ namespace FriendOrganizer.UI.Startup
 
             builder.RegisterType<LookupDataService>().AsImplementedInterfaces();
             builder.RegisterType<FriendRepository>().As<IFriendRepository>();
+            builder.RegisterType<MeetingRepository>().As<IMeetingRepository>();
 
             return builder.Build();
         }
