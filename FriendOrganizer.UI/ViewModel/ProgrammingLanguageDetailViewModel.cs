6f5bca5d7fc17f4b7dd7ffa8a917c093a1b65e0b
danieljaller <danieljaller@gmail.com>
2017-11-20 14:15:31 +0100

    Replaced the dialog messages with the mahapp version of it.

--- FriendOrganizer.UI/ViewModel/ProgrammingLanguageDetailViewModel.cs
+++ FriendOrganizer.UI/ViewModel/ProgrammingLanguageDetailViewModel.cs
@@ -105,7 +105,7 @@ namespace FriendOrganizer.UI.ViewModel
                 {
                     e = e.InnerException;
                 }
-                MessageDialogService.ShowInfoDialog("Error while saving the entities, " +
+                await MessageDialogService.ShowInfoDialogAsync("Error while saving the entities, " +
                                                     "the data will be reloaded. Details: " + e.Message);
                 await LoadAsync(Id);
 
@@ -129,7 +129,7 @@ namespace FriendOrganizer.UI.ViewModel
                     SelectedProgrammingLanguage.Id);
             if (isReferenced)
             {
-                MessageDialogService.ShowInfoDialog($"The language {SelectedProgrammingLanguage.Name}" +
+                await MessageDialogService.ShowInfoDialogAsync($"The language {SelectedProgrammingLanguage.Name}" +
                                                     $" can't be removed, as it is referenced by at least one friend");
                 return;
             }
