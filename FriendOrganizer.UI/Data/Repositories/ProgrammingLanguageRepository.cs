fd8cb88a182e08892601b77f67ed31a680c97bc6
danieljaller <danieljaller@gmail.com>
2017-11-20 10:26:22 +0100

    Added new method to GenericRepository and implemented ProgrammingLanguageRepository

---
+++ FriendOrganizer.UI/Data/Repositories/ProgrammingLanguageRepository.cs
@@ -0,0 +1,17 @@
+﻿using FriendOrganizer.DataAccess;
+using FriendOrganizer.Model;
+using System.Data.Entity;
+using System.Threading.Tasks;
+
+namespace FriendOrganizer.UI.Data.Repositories
+{
+  public class ProgrammingLanguageRepository
+  : GenericRepository<ProgrammingLanguage, FriendOrganizerDbContext>,
+    IProgrammingLanguageRepository
+  {
+    public ProgrammingLanguageRepository(FriendOrganizerDbContext context)
+      : base(context)
+    {
+    }
+  }
+}
--- FriendOrganizer.UI/Startup/Bootstrapper.cs
+++ FriendOrganizer.UI/Startup/Bootstrapper.cs
@@ -35,6 +35,7 @@ namespace FriendOrganizer.UI.Startup
             builder.RegisterType<LookupDataService>().AsImplementedInterfaces();
             builder.RegisterType<FriendRepository>().As<IFriendRepository>();
             builder.RegisterType<MeetingRepository>().As<IMeetingRepository>();
+            builder.RegisterType<ProgrammingLanguageRepository>().As<IProgrammingLanguageRepository>();
 
             return builder.Build();
         }
